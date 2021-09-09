c20a422893d6f623a47ab51a467d5e6e43b55681
Peyton <pekellogg@hotmail.com>
2021-09-09 15:43:33 -0700

    commit tree 8f7a30716dd847a0a9404a8f847f10d8ea6214d7

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+    size = array.size
+    case array.any?
+    when size == 1
+        array.join
+    when size == 2
+        array.join(" and ")
+    when size == 3
+        "#{array[0]}, #{array[1]}, and #{array[2]}"
+    when size >= 4
+        array[0..-2].join(", ") + ", and " + array[-1]
+    else
+        "Error: empty array"
+    end
 end
